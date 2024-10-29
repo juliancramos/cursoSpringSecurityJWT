4f12346bbda76f62ad1436202c9978126a98f807
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-28 23:06:12 -0500

    Commit inicial

---
+++ src/main/java/org/app/cursospringsecurityjwt/CursoSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package org.app.cursospringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoSpringSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/app/cursospringsecurityjwt/CursoSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.cursospringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
